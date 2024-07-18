24889fb8c16bed8315b64582e96d4a827568f973
Luis M. Rodriguez-R <lmrodriguezr@gmail.com>
2024-07-18 21:20:29 +0200

    Update NonpareilCurves.R
    Address #63

--- utils/NonpareilCurves.R
+++ utils/NonpareilCurves.R
@@ -81,21 +81,36 @@ for (i in sapply(optopt, function(x) sub("^--", "", x[["opt_str"]])))
 # Do the work
 
 ##
-# Function by @fgvieira via https://github.com/lmrodriguezr/nonpareil/issues/63
+# Function by @fgvieira via https://github.com/lmrodriguezr/nonpareil/issues/63,
+# as modified and available in https://github.com/MultiQC/MultiQC with some
+# minor additional modifications
 export_curve <- function(object){
   # Extract variables
-  n <- names(attributes(object))[c(1:12,21:29)]
-  x <- sapply(n, function(v) attr(object,v))
+  n <- names(attributes(object))[c(1:12, 21:29)]
+  x <- sapply(n, function(v) attr(object, v))
   names(x) <- n
   # Extract vectors
   n <- names(attributes(object))[13:20]
-  y <- lapply(n, function(v) attr(object,v))
+  y <- lapply(n, function(v) attr(object, v))
   names(y) <- n
-  append(x, y)
+  curve_json <- c(x, y)
+
+  # Add model
+  if (object$has.model) {
+    x_min   <- 1e3
+    x_max   <- signif(tail(attr(object, "x.adj"), n = 1) * 10, 1)
+    x.model <- exp(seq(log(x_min), log(x_max), length.out = 1e3))
+    y.model <- predict(object, lr = x.model)
+    curve_json <- append(curve_json, list(x.model = x.model))
+    curve_json <- append(curve_json, list(y.model = y.model))
+  }
+
+  curve_json
 }
 
 ##
-# Function by @fgvieira via https://github.com/lmrodriguezr/nonpareil/issues/63
+# Function by @fgvieira via https://github.com/lmrodriguezr/nonpareil/issues/63,
+# also available in https://github.com/MultiQC/MultiQC
 export_set <- function(object){
   y <- lapply(object$np.curves, "export_curve")
   names(y) <- sapply(object$np.curves, function(n) n$label)
